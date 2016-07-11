b53d673e6487a1b9dd9bab9c580280bfd7b23b5c
kshyatt <kshyatt@physics.ucsb.edu>
2016-07-11 15:33:15 -0700

    Got test tests working!!!

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,7 @@ end
 move_to_node1("compile")
 move_to_node1("enums")
 move_to_node1("docs")
+move_to_node1("test")
 move_to_node1("dates/periods")
 move_to_node1("strings/basic")
 move_to_node1("strings/types")
@@ -75,7 +76,7 @@ cd(dirname(@__FILE__)) do
         end
         push!(results, (t, resp))
     end
-
+    println("Done running tests")
     o_ts = Base.Test.DefaultTestSet("Overall")
     Base.Test.push_testset(o_ts)
     for res in results
